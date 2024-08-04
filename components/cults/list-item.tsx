76b259ea3613c0053aed750255f14401a8ee08d6
tBaronDar <thedarelis@gmail.com>
2024-08-04 11:50:13 +0300

    css adjustments for the list items

--- components/cults/list-item.tsx
+++ components/cults/list-item.tsx
@@ -1,16 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DummyItem } from "@/data/data";
 import { CultsContext } from "@/store/cults-context";
 
+import styles from "@/components/cults/list-item.module.css";
+
 function ListItem({ inputItem }: { inputItem: DummyItem }) {
 	const { setSelectedCult, selectedCult } = useContext(CultsContext);
+	const [isFocus, setIsFocus] = useState(false);
 
 	function cultClickHandler() {
 		setSelectedCult(inputItem);
 		console.log(selectedCult);
 	}
+
+	function focusHandler() {
+		setIsFocus(true);
+	}
+
+	let decoration = "cult";
+	if (isFocus) {
+		decoration = "focused";
+	}
+
 	return (
-		<li onClick={cultClickHandler}>
+		<li
+			onClick={cultClickHandler}
+			className={styles[decoration]}
+			onFocus={focusHandler}>
 			<h3>{inputItem.name}</h3>
 			<p>{inputItem.description}</p>
 		</li>
